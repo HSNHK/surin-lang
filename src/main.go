3cfb288a8f93baa8de39e27f7b0a2c308878d900
HSNHK <hsnmm1379@gmail.com>
2020-10-02 16:18:47 +0330

    create main

---
+++ src/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+func main(){
+	input:=os.Args
+	if len(input)>1{
+		file,err:=os.Open(input[1])
+		if err!=nil{
+			log.Fatal(fmt.Sprintf("[%s]"," not open source code"))
+		}
+		fileScan :=bufio.NewScanner(file)
+		for fileScan.Scan(){
+			fmt.Println(fileScan.Text())
+		}
+		if err:= fileScan.Err();err!=nil{
+			log.Fatal(fmt.Sprintf("[%s]",err))
+		}
+
+	}else {
+		command:=map[string]string{
+			"exit":"enter exit() for close in interpreter",
+			"hello":"hello programmer welcome to surin interpreter",
+			"help":"pales view README",
+
+		}
+
+		fmt.Println("   _____            _          __")
+		fmt.Println("  / ___/__  _______(_)___     / /   ____ _____  ____ _")
+		fmt.Println("  \\__ \\/ / / / ___/ / __ \\   / /   / __ `/ __ \\/ __ `/")
+		fmt.Println(" ___/ / /_/ / /  / / / / /  / /___/ /_/ / / / / /_/ /")
+		fmt.Println("/____/\\__,_/_/  /_/_/ /_/  /_____/\\__,_/_/ /_/\\__, /")
+		fmt.Println("                                             /____/")
+		fmt.Println("Hello Welcome to surin interpreter\n")
+
+		var userCommand string
+		for true {
+
+			fmt.Print(">>~#>")
+			fmt.Scan(&userCommand)
+
+			for key,value:=range command{
+				if userCommand==key{
+					fmt.Println(value)
+				}
+			}
+
+			if userCommand=="exit()"{
+				break
+			}
+
+		}
+	}
+}
