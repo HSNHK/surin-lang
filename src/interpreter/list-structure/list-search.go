a781971c121c4f750ac5c00728a76d2cf3bef21d
HSNHK <hsnmm1379@gmail.com>
2020-11-01 23:10:57 +0330

    update sort

--- src/interpreter/list-structure/list-search.go
+++ src/interpreter/list-structure/list-search.go
@@ -5,23 +5,22 @@ import "fmt"
 func sort(name string,ListMap *map[string][]interface{}){
 	var list=*ListMap
 	for index,value:=range list[name]{
-		if fmt.Sprintf("%T",value)=="string"{
-			if fmt.Sprintf("%T",list[name][index+1])=="string"{
-				if fmt.Sprintf("%v",value)[0]>
-					fmt.Sprintf("%v",list[name][index+1])[0]{
-					next:=list[name][index+1]
-					list[name][index+1]=value
-					list[name][index]=next
+		if fmt.Sprintf("%T",value)=="string" {
+			for i := index; i < len(list[name]); i++ {
+				if list[name][i]!=nil {
+					fmt.Println(list[name][i])
+					if fmt.Sprintf("%v", list[name][i]) >
+					   fmt.Sprintf("%v", list[name][i+1]){
+						next := list[name][i+1]
+						list[name][i+1] = list[name][i]
+						list[name][i] = next
+					}
 				}
-			}else if fmt.Sprintf("%T",list[name][index+1])=="int"{
-				next:=list[name][index+1]
-				list[name][index+1]=value
-				list[name][index]=next
 			}
 		}else if fmt.Sprintf("%T",value) =="int"{
 			for i:=index;i<=cap(list[name]);i++{
 				if fmt.Sprintf("%T",list[name][i+1])=="int"{
-					if fmt.Sprintf("%v",value)>
+					if fmt.Sprintf("%v",list[name][i])>
 						fmt.Sprintf("%v",list[name][i+1]){
 						next:=list[name][i+1]
 						list[name][i+1]=value
